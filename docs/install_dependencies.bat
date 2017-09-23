4d4b147ba2d4df94a0c83ac68958c971b267670f
Olivia <olivia_git@outlook.com>
2017-09-24 00:12:19 +0100

    More documentation.

--- docs/install_dependencies.bat
+++
@@ -1,5 +0,0 @@
-pip3 install Django==1.11.5
-pip3 install django-bootstrap3
-pip3 install imdbpie
-pip3 install simplejson
-pip install pdb
--- docs/install_script.bat
+++
@@ -1,6 +0,0 @@
-REM install Python virtual environment package
-pip3 install virtualenv 
-
-REM create and activate the venv
-virtualenv AVPlayer_venv
-AVPlayer_venv\Scripts\activate
